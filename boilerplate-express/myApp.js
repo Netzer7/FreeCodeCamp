f5f073bce8d861bef58a4a7b9104ee6fc23ddc4e
Netzer7 <jacobhastings7@gmail.com>
2023-04-14 18:53:10 -0700

    Add paraentheses

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -6,7 +6,7 @@ console.log("Hello world");
 app.use(express.static(__dirname + "/public"));
 
 app.use("/public",
-    express.static(__dirname + "/public");
+    express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
